2a495621f4a6b87143328f0b67714177ba53dad2
Johnston Family <familiarfletcher@gmail.com>
2019-01-28 19:14:37 -0700

    additional changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,38 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+<div>
+  <div class="header">
+    <nav>
+      <a href="">Home</a>
+      <a href="">Products</a>
+      <a href="">About Us</a>
+      <a href="">Contact Us</a>
+    </nav>
+    <div class='headerText'>
+      <img src="./../assets/textlogowhite.png" alt="Logo Text White">
+    </div>
+
   </div>
+  <div class='intro'>
+    <h2>Family Owned and Operated since 1992</h2>
+    <div class='products'>
+      <div>
+        <img src="./../assets/whistles.jpg" alt="Logo Text White">
+        <h3>Whistles</h3>
+        <p></p>
+      </div>
+      <div>
+        <img src="./../assets/buckle.jpg" alt="Logo Text White">
+        <h3>Buckles</h3>
+        <p></p>
+      </div>
+      <div>
+        <img src="./../assets/carvings.jpg" alt="Logo Text White">
+        <h3>Carvings</h3>
+        <p></p>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
@@ -41,18 +46,109 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+
+@font-face {
+  font-family: "Western";
+  src: url('./../assets/fonts/WEST____.TTF');
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+@font-face {
+  font-family: "Cowboy";
+  src: url('./../assets/fonts/Young Heart.ttf');
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+
+.header {
+  height: 99vh;
+  width: 100%;
+  background: linear-gradient(to bottom, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.5) ), url('./../assets/treesbkgd.jpg');
+  background-position: center;
+  background-size: cover;
+  background-repeat: none;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  display: flex;
+	flex-direction: column;
+	flex-wrap: nowrap;
+	justify-content: flex-start;
+	align-items: stretch;
+	align-content: stretch;
+  margin: 0;
+  position: initial;
+  text-align: center;
 }
-a {
-  color: #42b983;
+
+nav {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin: 4vh 0;
 }
+
+nav a {
+  width: 15%;
+  text-decoration: none;
+    color: white;
+    font-size: 30px;
+    margin: 0 4vw;
+    font-family: 'Poppins', sans-serif;
+    text-transform: uppercase;
+}
+
+.headerText {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.headerText>img {
+  max-width: 60%;
+}
+
+.intro {
+  background-color: linear-gradient(rgba(255, 255, 255, 0.8));
+  margin: 0 auto;
+}
+
+.intro h2 {
+  margin: 8vh 6vw;
+  font-size: 50px;
+  color: #151b1f;
+  font-family: "Cowboy", sans-serif;
+  text-align: left;
+  letter-spacing: 5px;
+}
+
+.products {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+}
+
+.products>div {
+  width: 33%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.products img {
+  width: 400px;
+  height: 400px;
+  border-radius: 100%;
+  margin-top: 5vh;
+}
+
+
+.products h3 {
+  font-size: 35px;
+  margin: 5vh 0;
+  color: #151b1f;
+  font-family: 'Poppins', sans-serif;
+  text-transform: uppercase;
+  letter-spacing: 3px;
+}
+
 </style>
